b27ee8d10da00e3314cba1c3876127692b59f1e5
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2026-05-18 08:50:19 +0530

    Improve dpop for NP AT token binding

--- component/org.wso2.carbon.identity.oauth2.dpop/src/main/java/org/wso2/carbon/identity/oauth2/dpop/dao/DPoPTokenManagerDAOImpl.java
+++ component/org.wso2.carbon.identity.oauth2.dpop/src/main/java/org/wso2/carbon/identity/oauth2/dpop/dao/DPoPTokenManagerDAOImpl.java
@@ -18,6 +18,9 @@
 
 package org.wso2.carbon.identity.oauth2.dpop.dao;
 
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.apache.commons.lang.StringUtils;
 import org.wso2.carbon.database.utils.jdbc.JdbcTemplate;
 import org.wso2.carbon.database.utils.jdbc.exceptions.DataAccessException;
 import org.wso2.carbon.identity.oauth.tokenprocessor.HashingPersistenceProcessor;
@@ -26,8 +29,12 @@ import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.dpop.constant.DPoPConstants;
 import org.wso2.carbon.identity.oauth2.dpop.util.Utils;
 import org.wso2.carbon.identity.oauth2.token.bindings.TokenBinding;
+import org.wso2.carbon.identity.oauth2.util.JWTUtils;
+import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
+import org.wso2.carbon.identity.oauth2.util.TokenMgtUtil;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class implements {@link DPoPTokenManagerDAO} interface.
@@ -41,6 +48,9 @@ public class DPoPTokenManagerDAOImpl implements DPoPTokenManagerDAO {
         if (refreshToken == null) {
             throw new IdentityOAuth2Exception("Refresh token cannot be null.");
         }
+        if (JWTUtils.isJWT(refreshToken)) {
+            return getTokenBindingFromRefreshTokenJWT(refreshToken);
+        }
 
         JdbcTemplate jdbcTemplate = Utils.getNewTemplate();
 
@@ -49,8 +59,11 @@ public class DPoPTokenManagerDAOImpl implements DPoPTokenManagerDAO {
 
         try {
             String finalRefreshToken = refreshToken;
+            String retrieveTokenBindingQuery = OAuth2Util.isAccessTokenPersistenceEnabled() ?
+                    SQLQueries.RETRIEVE_TOKEN_BINDING_BY_REFRESH_TOKEN_HASH :
+                    SQLQueries.RETRIEVE_TOKEN_BINDING_BY_REFRESH_TOKEN_HASH_NON_PERSISTENT_ACCESS_TOKEN;
             List<TokenBinding> tokenBindingList = jdbcTemplate.executeQuery(
-                    SQLQueries.RETRIEVE_TOKEN_BINDING_BY_REFRESH_TOKEN_HASH,
+                    retrieveTokenBindingQuery,
                     (resultSet, rowNumber) -> {
                         TokenBinding tokenBinding = new TokenBinding();
                         tokenBinding.setBindingType(resultSet.getString(1));
@@ -71,5 +84,36 @@ public class DPoPTokenManagerDAOImpl implements DPoPTokenManagerDAO {
                     refreshToken);
             throw new IdentityOAuth2Exception(error, e);
         }
+    }
+
+    private TokenBinding getTokenBindingFromRefreshTokenJWT(String refreshToken) throws IdentityOAuth2Exception {
+
+        SignedJWT signedJWT = TokenMgtUtil.parseJWT(refreshToken);
+        JWTClaimsSet claimsSet = TokenMgtUtil.getTokenJWTClaims(signedJWT);
+        Object bindingTypeObj = claimsSet.getClaim("binding_type");
+        Object bindingRefObj = claimsSet.getClaim("binding_ref");
+        if (bindingTypeObj == null && bindingRefObj == null) {
+            return null;
+        }
+        if (bindingTypeObj == null || bindingRefObj == null || StringUtils.isBlank(bindingRefObj.toString()) ||
+                !DPoPConstants.DPOP_TOKEN_TYPE.equals(bindingTypeObj.toString())) {
+            throw new IdentityOAuth2Exception("Malformed DPoP token binding claims found in the refresh token.");
+        }
+
+        TokenBinding tokenBinding = new TokenBinding();
+        tokenBinding.setBindingType(bindingTypeObj.toString());
+        tokenBinding.setBindingReference(bindingRefObj.toString());
+        tokenBinding.setBindingValue(getDPoPBindingValue(claimsSet));
+        return tokenBinding;
+    }
+
+    private String getDPoPBindingValue(JWTClaimsSet claimsSet) {
+
+        Object cnfObj = claimsSet.getClaim(DPoPConstants.CNF);
+        if (!(cnfObj instanceof Map)) {
+            return null;
+        }
+        Object bindingValue = ((Map<?, ?>) cnfObj).get(DPoPConstants.JWK_THUMBPRINT);
+        return bindingValue == null ? null : bindingValue.toString();
     }
 }
